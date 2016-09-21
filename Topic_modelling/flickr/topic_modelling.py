7196f7d01bd07c1dca50a09ef9b36cdd052a1c26
yash0307 <yash.patel@students.iiit.ac.in>
2016-09-21 13:19:09 +0200

    flickr topic modelling scripts

--- Topic_modelling/flickr/topic_modelling.py
+++ Topic_modelling/flickr/topic_modelling.py
@@ -7,24 +7,20 @@
 
 import json
 from nltk import word_tokenize
-from nltk.corpus import stopwords
 import gensim
 import logging
 import sys
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-fp = open('/home/yash/Data/data.json','r')
+fp = open('/home/yash/Data/flickr/data.json','r')
 data = json.load(fp)
 data_corpus = []
+print str(len(data.keys()))
 for i in data.keys():
     val = data[i]
     data_corpus.append(val)
 
-tok_corpus = []
-for i in data_corpus:
-    temp_tokens = [j.lower() for j in word_tokenize(i)]
-    tok_corpus.append(temp_tokens)
 
 id2word = gensim.corpora.Dictionary.load_from_text('/home/yash/DynamicLexiconGenerationCVC/Dataset_Dictionary/gensim_flickr.txt')
-corpus = [id2word.doc2bow(text) for text in tok_corpus]
+corpus = [id2word.doc2bow(text) for text in data_corpus]
 gensim.corpora.MmCorpus.serialize('/home/yash/Data/flickr/corpus.mm',corpus)
